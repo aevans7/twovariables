3df340653830f161680bd40ccae396e885024598
Alex <alex.c.evans1@gmail.com>
2020-03-02 21:20:50 -0500

    no change

--- src/main/java/com/ae/twovariables/homeController.java
+++ src/main/java/com/ae/twovariables/homeController.java
@@ -2,13 +2,11 @@ package com.ae.twovariables;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 @Controller
 public class homeController {
     public String homePage(Model model) {
         model.addAttribute("firstVar", "This is the first variable for exercise 1.02");
         model.addAttribute("secondVar", "This is the second variable for exercise 1.02");
         return "homePage";
-
     }
 }
