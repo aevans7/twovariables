24383eaf77c4b602e4b0617db2af0016591bdcff
Alex <alex.c.evans1@gmail.com>
2020-03-02 21:12:43 -0500

    Initial commit

---
+++ src/main/java/com/ae/twovariables/TwovariablesApplication.java
@@ -0,0 +1,13 @@
+package com.ae.twovariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwovariablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwovariablesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ae/twovariables/TwovariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.twovariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwovariablesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
